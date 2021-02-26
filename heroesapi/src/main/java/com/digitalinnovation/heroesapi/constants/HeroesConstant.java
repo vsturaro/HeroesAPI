6ee06867692eead8c54802491b12733582f5029c
vsturaro <vstecnologia@gmail.com>
2021-02-26 11:44:53 -0400

    API Heroes Livecoding DIO

---
+++ heroesapi/src/main/java/com/digitalinnovation/heroesapi/constants/HeroesConstant.java
@@ -0,0 +1,7 @@
+package com.digitalinnovation.heroesapi.constants;
+
+public class HeroesConstant {
+    public static final String HEROES_ENDPOINT_LOCAL="/heroes";
+    public static final String ENDPOINT_DYNAMO="https://localhost:8000";
+    public static final String REGION_DYNAMO="us-eust-1";
+}
